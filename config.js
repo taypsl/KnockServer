ce75c69ce5f8ad8acbf786b3d70821f28f287c04
System <none>
2017-04-11 06:04:30 +0000

    🌊🎷 Updated with Glitch

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                      'mongodb://hello:userPassword@ds139438.mlab.com:39438/knockknock';
-exports.TEST_DATABASE_URL = (
-	process.env.TEST_DATABASE_URL ||
-	'mongodb://user:user1password@ds113580.mlab.com:13580/dontknockittillyoutestit');
+                       global.DATABASE_URL;
+exports.TEST_DATABASE_URL = (process.env.TEST_DATABASE_URL);
 exports.PORT = process.env.PORT || 8080;
